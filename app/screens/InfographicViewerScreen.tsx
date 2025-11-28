bba0388b113d7ca3f306099cb50f22fa8f529551
Nicolas <nicolaspaulik97@gmail.com>
2025-11-28 15:50:11 +0100

    Dummy Screens erstellt

---
+++ app/screens/InfographicViewerScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function InfographicViewerScreen() {
+  return (
+    <View>
+      <Text>InfographicViewer Screen</Text>
+    </View>
+  );
+}
